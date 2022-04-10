ae2ba6d9e5e88d79f7bdd942d472ccfd0feaef29
Igor Dedkovskiy <divmy@yandex.ru>
2022-04-10 13:05:39 +0300

    Dumb commit

--- test_ip_filter.cpp
+++ test_ip_filter.cpp
@@ -1,6 +1,14 @@
 #include "gtest/gtest.h"
 #include "ip_filter.h"
 
+//#ifdef WIN32
+//constexpr unsigned long long md5_h = 0x36b72c25de983078;
+//constexpr unsigned long long md5_l = 0xb68625b7610e7673;
+//#else
+//constexpr unsigned long long md5_h = 0x24e7a7b2270daee8;
+//constexpr unsigned long long md5_l = 0x9c64d3ca5fb3da1a;
+//#endif
+
 TEST(TEST_FILTER, test_ip_filter)
 {
     ASSERT_TRUE(true);
