70decd9a6bf83b9102b010dc5fd24fe2d9bc00f1
Igor Dedkovskiy <78168238+igordedkovskiy@users.noreply.github.com>
2022-04-11 17:54:07 +0300

    Update check_md5sum.sh

--- check_md5sum.sh
+++ check_md5sum.sh
@@ -1,14 +1,12 @@
-#!/bin/sh
 #!/bin/bash
+#!/bin/sh
 
 answer=$(cat ./ip_filter.tsv | ./ip_filter | md5sum)
 answer=${answer:0:32}
-#echo $answer
 md5="24e7a7b2270daee89c64d3ca5fb3da1a"
 
-#if [ "$answer" -ne "$md5" ] # for integers
-if [ "$answer" != "$md5" ]; then # for strings
-	exit 1
+if [ "$answer" != "$md5" ]; then
+    exit 1
 fi
 
 exit 0
