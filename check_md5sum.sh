a9558a3c479d1472350a2aabe01fbb09b6fb6eab
Igor Dedkovskiy <78168238+igordedkovskiy@users.noreply.github.com>
2022-04-11 17:50:34 +0300

    Fix check_md5sum.sh

--- check_md5sum.sh
+++ check_md5sum.sh
@@ -6,6 +6,9 @@ answer=${answer:0:32}
 #echo $answer
 md5="24e7a7b2270daee89c64d3ca5fb3da1a"
 
-if [ "$answer" -ne "$md5" ]
-    exit 1
+#if [ "$answer" -ne "$md5" ] # for integers
+if [ "$answer" != "$md5" ]; then # for strings
+	exit 1
+fi
+
 exit 0
