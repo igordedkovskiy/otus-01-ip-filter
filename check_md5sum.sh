b948b0903ce1391a3bc3472a9c71fbc6aeffd824
Igor Dedkovskiy <divmy@yandex.ru>
2022-04-12 18:30:44 +0300

    Add check_md5.cmd - md5 check for windows

--- check_md5sum.sh
+++ check_md5sum.sh
@@ -1,10 +1,17 @@
-#!/bin/sh
 #!/bin/bash
+#!/bin/sh
 
-answer=$(cat ./ip_filter.tsv | ./ip_filter | md5sum)
+answer=$(cat ./ip_filter.tsv | ./Release/ip_filter | md5sum)
+answer=${answer:0:32}
+#md5="24e7a7b2270daee89c64d3ca5fb3da1a"
+md5="36b72c25de983078b68625b7610e7673"
+
 echo $answer
-md5="24e7a7b2270daee89c64d3ca5fb3da1a"
+echo $md5
 
-if [ "$answer" -ne "$md5" ]
-    exit -1
-exit 1
+#if [ "$answer" -ne "$md5" ] # for integers
+if [ "$answer" != "$md5" ]; then # for strings
+	exit 1
+fi
+
+exit 0
