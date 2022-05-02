0222cec20ee3418f64ce693b22fbb4881051f03b
Igor Dedkovskiy <divmy@yandex.ru>
2022-05-02 16:38:49 +0300

    Change sort test

--- test_ip_filter.cpp
+++ test_ip_filter.cpp
@@ -16,14 +16,14 @@ using namespace ip_filter;
 
 TEST(TEST_ADDR_SORT, test_addr_sort)
 {
-    addr_collection_t ip_list;
-    std::stringstream input{"113.162.145.156	111	0\n"
-                            "1.39.22.221	5	6\n"
-                            "113.55.46.91	0	2\n"
-                            "113.55.46.98	0	2\n"
-                            "113.55.42.91	0	2\n"
-                            "1.39.22.224	5	6\n"
-                            "46.39.22.224	5	6\n"
+    std::istringstream input{"7\n"
+                             "113.162.145.156	111	0\n"
+                             "1.39.22.221	5	6\n"
+                             "113.55.46.91	0	2\n"
+                             "113.55.46.98	0	2\n"
+                             "113.55.42.91	0	2\n"
+                             "1.39.22.224	5	6\n"
+                             "46.39.22.224	5	6\n"
                            };
     addr_collection_t ans{{113,162,145,156},
                           {113,55,46,98},
@@ -33,16 +33,8 @@ TEST(TEST_ADDR_SORT, test_addr_sort)
                           {1,39,22,224},
                           {1,39,22,221}
                          };
-    unsigned int cntr = 0;
-    std::string read;
-    while(input >> read)
-    {
-        if(cntr++ % 3 == 0)
-            ip_list.emplace_back(parse_address(read));
-    }
-
+    auto [ip_list, failed_line] = ip_filter::read_input(input);
     sort(ip_list);
-
     ASSERT_TRUE(ip_list == ans);
 }
 
